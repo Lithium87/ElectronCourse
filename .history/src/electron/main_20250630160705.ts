216fe3370525e89f21b5eeb4ea522505d245fa4d
Lithium@87 <cibs@abv.bg>
2025-06-30 17:02:17 +0300

    Configure electron-builder

---
+++ .history/src/electron/main_20250630160705.ts
@@ -0,0 +1,12 @@
+import {app, BrowserWindow} from 'electron';
+
+import path from 'path';
+
+function createWindow () {
+  const win = new BrowserWindow ({
+    width: 800,
+    height: 600,
+  });
+  win.loadFile (path.join (app.getAppPath (), 'dist-react', 'index.html'));
+}
+app.whenReady ().then (createWindow);
--- src/electron/main.js
+++ src/electron/main.js
@@ -1,10 +1,10 @@
-import {app, BrowserWindow} from 'electron';
+import { app, BrowserWindow } from 'electron';
 import path from 'path';
-function createWindow () {
-  const win = new BrowserWindow ({
-    width: 800,
-    height: 600,
-  });
-  win.loadFile (path.join (app.getAppPath (), 'dist-react', 'index.html'));
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+    win.loadFile(path.join(app.getAppPath(), 'dist-react', 'index.html'));
 }
-app.whenReady ().then (createWindow);
+app.whenReady().then(createWindow);
